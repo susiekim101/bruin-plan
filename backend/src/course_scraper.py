d2ed05f23625186e5bb8182571d34974a2c85959
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-10-26 08:27:18 -0700

    add regex matching to extract course id and title

--- backend/src/course_scraper.py
+++ backend/src/course_scraper.py
@@ -1,4 +1,5 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 def getHTMLdocument(url):
@@ -9,8 +10,23 @@ url = "https://www.seasoasa.ucla.edu/curric-24-25/23-bioeng-ugstd-24.html"
 html_document = getHTMLdocument(url)
 
 soup = BeautifulSoup(html_document, "html.parser")
+course_names = []
+course_titles = []
 
 def scrape_course(soup):
     for ul in soup.find_all('ul', class_='no-bullets'):
         for list in ul.find_all('li'):
-            print(list.string)
+            course_match = re.search(r"^[A-Z 0-9& ]+(?= - )", list.string)
+            title_match = re.search(r"(?<= - )[A-Za-z(),-:' ]+$", list.string)
+            if course_match:
+                course_names.append(course_match.group())
+            else:
+                print("No match found: " + list.string)
+
+            
+            if title_match:
+                course_titles.append(title_match.group())
+            else:
+                print("No match found: " + list.string)
+
+
