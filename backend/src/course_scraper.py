1df3d44bc24bcfe78d275379617e0503bb4c39c2
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-10-26 00:23:06 -0700

    define function for scraping courses from website

--- backend/src/course_scraper.py
+++ backend/src/course_scraper.py
@@ -9,3 +9,8 @@ url = "https://www.seasoasa.ucla.edu/curric-24-25/23-bioeng-ugstd-24.html"
 html_document = getHTMLdocument(url)
 
 soup = BeautifulSoup(html_document, "html.parser")
+
+def scrape_course(soup):
+    for ul in soup.find_all('ul', class_='no-bullets'):
+        for list in ul.find_all('li'):
+            print(list.string)
