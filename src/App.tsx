986333cebdd899b28390f8a53490df93c349f3fb
jqin206 <juneqin2017@gmail.com>
2025-10-31 14:58:58 -0700

    create container for Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import TotalUnits from './TotalUnits/TotalUnits'
-import Share from './Share/Share'
+import Sidebar from './Sidebar/Sidebar'
+//import Share from './Share/Share'
 
 function App() {
 
   return (
     <>
       <TotalUnits units={10} />
-      <Share />
+      <Sidebar />
+      {/* <Share />*/}
     </>
   )
 }
