295f53595dd8665884347a2180ca94cc82e972c4
jqin206 <juneqin2017@gmail.com>
2025-11-02 14:40:04 -0800

    update css for Sidebar and SearchBar

--- src/Sidebar/SearchBar.tsx
+++ src/Sidebar/SearchBar.tsx
@@ -4,14 +4,12 @@ function SearchBar () {
     const [ searchTerm, setSearchTerm ] = useState('')
 
     return (
-        <div>
-            <input
-                className='h-8 w-4/5 bg-white px-2 py-1.5 rounded-2xl text-zinc-500'
-                placeholder="Search"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-            />
-        </div>
+        <input
+            className='h-8 w-full bg-white px-2 py-1.5 rounded-2xl text-zinc-500'
+            placeholder="Search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+        />
     )
 }
 
--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import SearchBar from './SearchBar'
 function Sidebar() {
     return (
         <>
-            <div className="bg-blue-800 rounded-l-3xl px-3 py-6 absolute inset-y-0 right-0 w-1/5">
+            <div className="flex justify-center bg-blue-800 rounded-l-3xl px-6 py-6 absolute inset-y-0 right-0 w-1/5">
                 <SearchBar />
             </div>
         </>
