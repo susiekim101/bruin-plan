cb401d1a71d78a18693df332578d0a319c2690c0
jqin206 <juneqin2017@gmail.com>
2025-11-02 15:18:21 -0800

    create Dashboard component

---
+++ src/Dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import TotalUnits from '../TotalUnits/TotalUnits.tsx'
+import Sidebar from '../Sidebar/Sidebar.tsx'
+
+function Dashboard () {
+    <>
+        <TotalUnits units={15} />
+        <Sidebar />
+    </>
+}
+
+export default Dashboard
