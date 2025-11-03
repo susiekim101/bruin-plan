57b44a9030ad242e8dfcab025c71f1872c9d9036
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-11-03 10:43:30 -0800

    remove absolute positioning, adjust sizing and flex

--- src/Dashboard/components/Sidebar/Sidebar.tsx
+++ src/Dashboard/components/Sidebar/Sidebar.tsx
@@ -1,17 +1,17 @@
-import Card from '../Dashboard/components/CourseCards/Card'
+import Card from '../CourseCards/Card'
 import SearchBar from './SearchBar'
 
 function Sidebar() {
     return (
-        <>
-            <div className="flex flex-col justify-center bg-blue-800 rounded-l-3xl px-6 py-6 absolute inset-y-0 right-0 w-1/5">
+        <div className="w-full flex justify-end">
+            <div className="flex flex-col justify-center bg-blue-800 rounded-l-3xl px-6 py-6 w-3xs h-screen">
                 <SearchBar />
                 <div className="flex flex-col gap-4 mt-6 overflow-y-auto h-full w-full">
                     <Card />
                     <Card />
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
