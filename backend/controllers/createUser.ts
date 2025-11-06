754e5e3b80c738033774f19d8bacee3f5e3b54b3
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-11-06 14:59:22 -0800

    write minimal code to pass 2 tests

--- backend/controllers/createUser.ts
+++ backend/controllers/createUser.ts
@@ -1,24 +1,23 @@
 // import { Request, Response } from 'express';
-import connection from "../src/database";
-// interface addUserProps {
-//     'first_name': string
-//     'last_name': string
-//     'email': string
-//     'password': string
-//     'major': string
-// }
-
-export async function createUser() {
-    return connection;
+import { connection } from "../src/database";
+interface createUserProps {
+    'first_name': string
+    'last_name': string
+    'email': string
+    'password': string
+    'major': string
 }
 
-// async function createUser(req: Request, res: Response) {
-//     // TODO
-//     // Get a reference to the database
+export async function createUser({ first_name, last_name, email, password, major}: createUserProps) {
+    // ({ first_name, last_name, email, password, major}: createUserProps
+    // Get a reference to the database
 //     // Find the majors table
 //         // Get the major's id
 //     // Find the Users table
 //     // Add entry (user_id, first_name, last_name, email, password_hash, major_id)
-//     console.log(req.body)
-//     res.send("Creating user")
-// }
+// Return the useId
+    const major_query = 'USE bruin_plan; SHOW Majors';
+    connection.execute(major_query);
+
+    return 1234;
+}
