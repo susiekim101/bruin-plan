2128e8b6dfbeef68beb9c058fcde2341b6b8ac7a
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-11-06 20:53:26 -0800

    add POST request to route for creating user

--- backend/routes/router.ts
+++ backend/routes/router.ts
@@ -1,11 +1,20 @@
 
 import { Router } from 'express'
+import { Request, Response } from 'express';
 import { createUser } from '../controllers/createUser';
 // Import other router files here
 // import userRoutes from '...'
 
 const router = Router();
-router.use('/createUser', createUser);
+
+router.post('/createUser', async (req: Request, res: Response) => {
+    try {
+        await createUser(req.body);
+        res.status(200).json({message: "Succesfully created user."});
+    } catch {
+        res.status(500).json({message: "Failed to create user from the server"});
+    }
+});
 // Add specific routes to router
 // router.use(userRoutes)
 
