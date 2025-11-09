74155533027e96fd624475fc72b7af10b64c6dbe
jqin206 <juneqin2017@gmail.com>
2025-11-09 10:26:27 -0800

    write code to pass first test

--- backend/controllers/loadCourseCards.ts
+++ backend/controllers/loadCourseCards.ts
@@ -1,1 +1,13 @@
 import { connection } from '../src/database'
+
+export async function loadCourseCards () {
+    let db_connection;
+    try {
+        db_connection = await connection.getConnection();
+    } catch (error) {
+        console.error('Error fetching data:', error)
+    } finally {
+        if (db_connection)
+            db_connection.release();
+    }
+}
--- backend/tests/loadCourseCards.test.cjs
+++ backend/tests/loadCourseCards.test.cjs
@@ -1,23 +1,26 @@
 // Create a mock execute function
 const mockExecute = jest.fn();
+const mockRelease = jest.fn();
+const mockGetConnection = jest.fn().mockResolvedValue({
+  execute: mockExecute,
+  release: mockRelease
+});
 
 // Create a mock connection from my database
-jest.mock('../src/database', () => {
-  return {
-    connection: {
-      execute: mockExecute
-    }
-  };
-});
+jest.mock('../src/database', () => ({
+  connection: {
+    getConnection: mockGetConnection
+  }
+}));
 
 const { loadCourseCards } = require('../controllers/loadCourseCards');
 
 // Start with clean state each time
 beforeEach(() => {
-  mockExecute.mockClear()
+  jest.clearAllMocks();
 });
 
 test('loadCourseCards should estabish connection to database', async () => {
     const result = await loadCourseCards();
-    expect(result).toBe('');
+    expect(mockGetConnection).toHaveBeenCalledTimes(1);
 });
