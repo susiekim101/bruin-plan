fbac38d8e6d1829a257b4d4e079c9245237c96b5
Sarah Johal <sarahjohal10@icloud.com>
2025-11-08 20:21:42 -0800

    created separate course cards and custom cards

---
+++ src/Dashboard/components/CourseCards/CustomCards.tsx
@@ -0,0 +1,48 @@
+import CourseUnits from './CourseUnits.tsx';
+import CourseStatus from './CourseStatus.tsx';
+import CourseClassification from './CourseClassification.tsx';
+import CourseName from './CourseName.tsx';
+import CourseTitle from './CourseTitle.tsx';
+
+function CustomCard() {
+    return (
+        <>
+            <div className="
+                bg-sky-600 rounded-[20px] border border-black w-fit h-fit flex flex-col p-3
+            ">
+                <div className="
+                    p-1 flex justify-between text-amber-300 font-bold text-[12px]
+                ">
+                    <div>
+                        <p>
+                            Units: {<CourseUnits />}
+                        </p>
+                    </div>
+                    <div 
+                        className="bg-zinc-100 text-gray-600 rounded-md text-[10px] font-normal inline-block h-fit ml-auto items-center
+                    ">
+                        <CourseClassification />
+                    </div>
+                </div>
+                <div 
+                    className="p-1 pt-2 flex flex-start text-white font-bold text-[18px] w-full
+                ">
+                    <CourseName />
+                </div>
+                <div 
+                    className="p-1 pt-0 flex flex-start text-white font-normal text-[16px] w-full
+                ">
+                    <CourseTitle />
+                </div>
+                <div className="
+                    bg-zinc-100 rounded-md text-gray-600 text-[10px] font-normal ml-auto max-w-min flex items-center
+                ">
+                    <CourseStatus />
+                </div>
+            </div>
+        </>
+    );
+};
+
+
+export default CustomCard;
--- src/Dashboard/components/Sidebar/Sidebar.tsx
+++ src/Dashboard/components/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
-import Card from '../CourseCards/Card'
-import SearchBar from './SearchBar'
+import CustomCard from '../CourseCards/CustomCards';
+import CourseCard from '../CourseCards/CourseCards';
+import SearchBar from './SearchBar';
 
 function Sidebar() {
     return (
@@ -7,8 +8,9 @@ function Sidebar() {
             <div className="flex flex-col justify-center bg-blue-800 rounded-l-3xl px-6 py-6 w-3xs h-screen">
                 <SearchBar />
                 <div className="flex flex-col gap-4 mt-6 overflow-y-auto h-full w-full">
-                    <Card />
-                    <Card />
+                    <CustomCard />
+                    <CustomCard />
+                    <CourseCard units={4} courseName='COM SCI 35L' courseTitle='Software Construction' courseClassification='Major'/>
                 </div>
             </div>
         </div>
