faba2ba25690ed873e507a4bb518cbd4bf94b4e6
jqin206 <juneqin2017@gmail.com>
2025-11-09 10:52:03 -0800

    refactor by adding comments

--- backend/controllers/loadCourseCards.ts
+++ backend/controllers/loadCourseCards.ts
@@ -1,16 +1,20 @@
 import { connection } from '../src/database'
 
-export async function loadCourseCards (major_id: number) {
+export async function loadCourseCards (userMajorID: number) {
     let db_connection;
     try {
+        // connect to database
         db_connection = await connection.getConnection();
+
+        // query Courses table for courses with major_id = userMajorID
         const query = `SELECT * FROM Courses WHERE major_id = ?`;
-        const [rows] = await db_connection.execute(query, [major_id]);
+        const [rows] = await db_connection.execute(query, [userMajorID]);
         return rows;
 
     } catch (error) {
         console.error('Error fetching data:', error)
     } finally {
+        // if connection established, release connection
         if (db_connection)
             db_connection.release();
     }
