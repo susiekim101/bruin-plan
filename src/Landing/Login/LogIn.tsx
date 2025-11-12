ada1a24e86b733a6c12fa535d95666775f35537f
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-11-11 22:37:16 -0800

    add toggling logic for login/signup

--- src/Landing/Login/LogIn.tsx
+++ src/Landing/Login/LogIn.tsx
@@ -1,11 +1,11 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { X } from 'lucide-react';
 
 function LogIn() {
     const dialogRef = useRef<HTMLDialogElement>(null);
+    const [signup, setSignUp] = useState(true);
 
     function handleOpenClick() {
-        // TODO: HANDLE CLICK FOR LOG IN. CREATE DIALOG.
         if(dialogRef.current) {
             dialogRef.current.showModal();
         }
@@ -22,7 +22,8 @@ function LogIn() {
             <dialog ref={dialogRef} className="p-0 rounded-lg shadow-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
                 <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs h-fit border p-4">
                     <header className="flex justify-between">
-                        <legend className="text-lg">Create Account</legend>
+                        
+                        <legend className="text-lg" id="registration-title">{signup ? "Create Account" : "Log In"}</legend>
                         <X className="w-5 h-5 cursor-pointer" onClick={handleCloseClick}/>
                     </header>
 
@@ -32,11 +33,27 @@ function LogIn() {
                     <label className="label">Password</label>
                     <input type="password" className="input" placeholder="Password" />
 
-                    <button className="btn btn-neutral mt-4">Create Account</button>
+                    {signup && 
+                    <>
+                        <label className="label">Major</label>
+                        <select id="major-input" defaultValue="Select your major" className="select">
+                            <option disabled={true}>Select your major</option>
+                            <option>Bioengineering</option>
+                            <option>Computer Science</option>
+                            <option>Computer Science and Engineering</option>
+                            <option>Computer Engineering</option>
+                        </select>
+                    </>}
+                    
+                    <button className="btn btn-neutral mt-4">{signup ? "Create Account" : "Log In"}</button>
+                    <div className="flex gap-2 justify-center">
+                        <p>{signup ? "Already have an account?" : "Don't have an account?"}</p>
+                        <p className="cursor-pointer" onClick={() => setSignUp(!signup)}>{signup ? "Log in" : "Sign up"}</p>
+                    </div>
                 </fieldset>
             </dialog>
 
-            <button className="w-fit h-fit text-white py-1 px-2 text-sm border rounded-md border-blue-800 bg-blue-800 cursor-pointer" onClick={handleOpenClick}>My Dashboard</button>
+            <button className="w-fit h-fit text-white py-1 px-2 text-sm border rounded-md border-blue-800 bg-blue-800 cursor-pointer" onClick={handleOpenClick} id="my-dashboard">My Dashboard</button>
         </div>
     );
 }
