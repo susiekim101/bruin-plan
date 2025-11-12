47b214e202c5c32c5bc7a0937d86060ecde91d33
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-11-11 18:05:17 -0800

    dummy text to Dashboard

--- src/Landing/Landing.tsx
+++ src/Landing/Landing.tsx
@@ -1,10 +1,15 @@
+import { Link } from "react-router-dom";
 import LogIn from "./LogIn/LogIn";
+
 function Landing() {
 
     return (
         <div className="w-full h-screen flex flex-col justify-center items-center">
             <h1>Bruin Plan</h1>
             <LogIn/>
+            <nav>
+                <Link to='/dashboard'>Dashboard</Link>
+            </nav>
         </div>
     );
 }
