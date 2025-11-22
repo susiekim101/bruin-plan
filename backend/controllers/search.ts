ff5b15bce01c43677bd09f811c41b91044e60dfe
jqin206 <juneqin2017@gmail.com>
2025-11-22 14:41:17 -0800

    create controller for search bar

--- backend/controllers/search.ts
+++ backend/controllers/search.ts
@@ -1,12 +1,12 @@
 import { connection } from '../src/database.ts';
 
-export async function fetchCoursesByMajor (userMajorID: number) {
+export async function search (searchTerm: string) {
     try {
-        // Connec to db in database.ts?
+        // Connect to db in database.ts?
         const db = await connection.getConnection();
         // query Courses table for courses with major_id = userMajorID
-        const query = `SELECT * FROM Courses WHERE major_id = ?`;
-        const [rows] = await db.execute(query, [userMajorID]);
+        const query = `SELECT * FROM Courses WHERE course_number LIKE %?%`;
+        const [rows] = await db.execute(query, [searchTerm]);
         
         return rows;
     } catch (error) {
