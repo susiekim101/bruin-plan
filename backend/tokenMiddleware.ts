336d9b6da7c7e68e46ec6257c49136d034346ddb
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-11-21 22:53:35 -0800

    add token verification to coursesRouter before rendering courses in sidebar

---
+++ backend/tokenMiddleware.ts
@@ -0,0 +1,23 @@
+import type { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import 'dotenv/config';
+
+// Extend Express Request interface to include 'user'
+
+const verifyToken = (req: Request, res: Response, next: () => void) => {
+    const token = req.cookies.token;
+
+    if(!token) {
+        return res.status(401).json({ message: 'No JWT token.' });
+    }
+
+    try {
+        jwt.verify(token, process.env.JWT_SECRET as string);
+        // req.user = payload;
+        next();
+    } catch {
+        return res.status(403).json({ message: 'Invalid token.' });
+    }
+};
+
+export default verifyToken;
