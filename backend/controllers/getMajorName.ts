86056d0b74c68195fb42cdd6b9e51a17615e0641
jqin206 <juneqin2017@gmail.com>
2025-11-25 17:15:24 -0800

    write code to pass previous test

--- backend/controllers/getMajorName.ts
+++ backend/controllers/getMajorName.ts
@@ -1,9 +1,13 @@
 import { connection } from '../src/database.ts';
 
-export async function getMajorName() {
+export async function getMajorName(userMajorID: Number) {
     try {
         // Connect to db in database.ts
         const db = await connection.getConnection();
+        const query = `SELECT major_name FROM Majors WHERE major_id = ?`;
+        const [rows] = await db.execute(query, [userMajorID]);
+        
+        return rows;
     } catch (error) {
         console.log('Error fetching data:', error);
     }
