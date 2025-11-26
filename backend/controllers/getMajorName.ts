6159705011435afd1586d0d8806b322a9e08eea1
jqin206 <juneqin2017@gmail.com>
2025-11-25 17:16:25 -0800

    refactor code by adding comments

--- backend/controllers/getMajorName.ts
+++ backend/controllers/getMajorName.ts
@@ -4,6 +4,8 @@ export async function getMajorName(userMajorID: Number) {
     try {
         // Connect to db in database.ts
         const db = await connection.getConnection();
+
+        // Query Majors table for major_name with major_id == userMajorID
         const query = `SELECT major_name FROM Majors WHERE major_id = ?`;
         const [rows] = await db.execute(query, [userMajorID]);
         
