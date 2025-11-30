84af427bafb13d80a755f1882b37ffff142b029e
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-11-29 17:54:11 -0800

    add unique plan_item_ids when fetching items

--- src/Public/components/FullPlan.tsx
+++ src/Public/components/FullPlan.tsx
@@ -8,6 +8,7 @@ type FullPlanProps = {
 };
 
 type PlanItems = {
+    plan_item_id: number,
     course_number: string,
     course_title: string,
     year: number,
@@ -19,42 +20,49 @@ function FullPlan({ plan_id, handleCloseClick }: FullPlanProps) {
 
    const dummyData: PlanItems[] = [
         {
+            plan_item_id: 1,
             course_number: "MATH 31A",
             course_title: "Calculus",
             year: 1,
             quarter: 'Fall',
         },
         {
+            plan_item_id: 2,
             course_number: "COM SCI 31",
             course_title: "Introduction to C++",
             year: 1,
             quarter: 'Fall',
         },
         {
+            plan_item_id: 3,
             course_number: "MATH 31B",
             course_title: "Multivariable Calculus",
             year: 1,
             quarter: 'Winter',
         },
         {
+            plan_item_id: 4,
             course_number: "COM SCI 32",
             course_title: "Data Structures & Algorithms",
             year: 1,
             quarter: 'Winter',
         },
         {
+            plan_item_id: 5,
             course_number: "MATH 32A",
             course_title: "Differential Equations",
             year: 1,
             quarter: 'Spring',
         },
         {
+            plan_item_id: 6,
             course_number: "COM SCI 33",
             course_title: "Operating Systems",
             year: 2,
             quarter: 'Fall',
         },
         {
+            plan_item_id: 7,
             course_number: "COM SCI M51A",
             course_title: "Logic Systems",
             year: 2,
