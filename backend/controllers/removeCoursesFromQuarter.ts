41308b2c4702188f7b25921c631fe80d2ece84ad
Sarah Johal <sarahjohal10@icloud.com>
2025-12-01 10:50:01 -0800

    updating types in controllers

--- backend/controllers/removeCoursesFromQuarter.ts
+++ backend/controllers/removeCoursesFromQuarter.ts
@@ -8,9 +8,17 @@ interface removeCourseProps {
   quarterName: "Fall" | "Winter" | "Spring" | "Summer";
 }
 
+interface PlanIdResult {
+    length: number;
+    plan_id: number,
+}
+
+interface DeleteCourseResult {
+  affectedRows: number;
+}
+
 export async function removeCoursesFromQuarter({ userId, courseId, yearIndex, quarterName }: removeCourseProps) {
-  console.log("goes to delete course");
-const results: any = await getPlanId({ userId: userId });
+const results: PlanIdResult[] = await getPlanId({ userId: userId });
   if (!results || results.length == 0 || results[0].length == 0)  {
     throw new Error('Cannot find planId');
   }
@@ -19,8 +27,7 @@ const results: any = await getPlanId({ userId: userId });
   const query = `
     DELETE FROM Plan_Items WHERE plan_id = ? AND course_id = ? AND year = ? AND quarter = ?;`;
 
-    console.log("gets to delete query");
-  const [deleteResult] : any = await connection.execute(query, [
+  const [deleteResult] : DeleteCourseResult[] = await connection.execute(query, [
     planId,
     courseId,
     yearIndex,
