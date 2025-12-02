a6e71c7f24f2709abeb1052db19a212bad67919e
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-12-01 17:31:45 -0800

    refactor with documentation

--- backend/controllers/fetchPlanItems.ts
+++ backend/controllers/fetchPlanItems.ts
@@ -8,28 +8,51 @@ interface getPlanItemsProps {
     plan_id: number
 };
 
+/* Retrieves all of the plans that are shared
+
+Returns an array of objects, where each object has a plan_id and major_id
+    e.g.
+    [
+        { plan_id: 1, major_id: 2 },
+        { plan_id: 2, major_id: 3 },
+        ...
+    ]
+*/
 export async function getAllPublicPlans() {
     try {
         const query = `SELECT plan_id, major_id FROM User_Plans WHERE is_shared = 1`;
         const [rows] = await connection.execute(query);
-        console.log("Retrieved all public plans: ", rows);
         return rows;
     } catch (err) {
         console.error(err);
     }
 }
 
+/*
+Retrieves the name of the major by its major_id
+Returns a row with a single object that has a major_name field
+    e.g. [ { major_name: "Computer Science"} ]
+*/
 export async function getMajorById({ major_id }: getMajorByIdProps) {
     try {
         const query = `SELECT major_name FROM Majors WHERE major_id = ?`;
         const [rows] = await connection.execute(query, [ major_id ]);
-        console.log(`getMajorById(${major_id}) returned ${rows}`);
         return rows;
     } catch (err) {
         console.error(`getMajorById(${major_id}) threw an error: ${err}`);
     }
 }
 
+/* Each user has a dedicated plan_id, and each plan_id has a plan_item for each individual course. Given a user's plan_id, return the information of each plan to display on a Course Card
+Returns an array of objects, where each object represents a single course in the user's plan
+    e.g. 
+    [
+        { plan_item_id: 1, course_number: "MATH 32A", course_name: "Multivariable Calculus", year: 1, quarter: "Fall" },
+        { plan_item_id: 2, course_number: "COM SCI 31", course_name: "Introduction to C++", year: 2, quarter: "Spring" },
+        ...
+    ]
+
+*/
 export async function getPlanItems({ plan_id }: getPlanItemsProps) {
     try {
         const query = `SELECT pi.plan_item_id, c.course_number, c.course_name, pi.year, pi.quarter FROM Plan_Items pi JOIN Courses c ON pi.course_id = c.course_id WHERE plan_id = ?`;
--- backend/controllers/sharePlan.ts
+++ backend/controllers/sharePlan.ts
@@ -5,15 +5,15 @@ interface SharePlanProps {
     'user_id': number
 }
 
-
-
+/*
+Update the is_shared boolean status to true in User_Plans
+No return value 
+*/
 export async function sharePlan({ user_id }: SharePlanProps) {
-    // change isShared to true
     try { 
         const query = `UPDATE User_Plans SET is_shared = true WHERE user_id = ?`;
 
         await connection.execute(query, [user_id]);
-        console.log("Update share_plan")
     } catch(err) {
         console.error(err);
     }
