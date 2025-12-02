59df3ad674dfb7655dc4819bf20fb05c94f990af
Sarah Johal <sarahjohal10@icloud.com>
2025-12-01 17:33:42 -0800

    added quarter unit total functionality to each quarter

--- src/Dashboard/Header/Header.tsx
+++ src/Dashboard/Header/Header.tsx
@@ -2,13 +2,14 @@ import TotalUnits from "../components/TotalUnits/TotalUnits";
 import Share from "../components/Share/Share";
 
 interface HeaderProps {
+    totalUnits: number,
     year: number,
 }
-export default function Header({year}: HeaderProps) {
+export default function Header({totalUnits, year}: HeaderProps) {
 
     return (
         <div className="w-full flex justify-between items-center px-3">
-            <TotalUnits units={10}/>
+            <TotalUnits units={totalUnits}/>
             <p className="text-4xl text-sky-800 font-bold">Year {year}</p>
             <Share/>
         </div>
