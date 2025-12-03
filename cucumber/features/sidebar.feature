e9c67faeeec8ab8487450f985b2d7c371ac1e5f0
jqin206 <juneqin2017@gmail.com>
2025-12-03 14:43:26 -0800

    condense Background steps for sidebar feature; add tag to sidebar.feature

--- cucumber/features/sidebar.feature
+++ cucumber/features/sidebar.feature
@@ -1,14 +1,11 @@
+@sidebar
 Feature: Sidebar display
     As a user of the Bruin Plan,
     I want to see the list of courses I need to take for my engineering major
     so that I can plan out my classes for each quarter of my 4 years at UCLA
     
     Background: User is logged in
-    Given I navigate to the login page
-    And I enter "testuser@ucla.edu" as email
-    And I enter "Password123!" as password
-    When I click the login button
-    Then I should be logged in successfully
+    Given I log in as a returning user
 
     Scenario: User should see all components of the Sidebar.
     When I am on the dashboard,
--- cucumber/steps/sidebar.step.js
+++ cucumber/steps/sidebar.step.js
@@ -9,17 +9,20 @@ Before({tags: "@sidebar" }, async function () {
     browser = await chromium.launch({ headless: false });
     const context = await browser.newContext();
     page = await context.newPage();
+});
 
+Given('I log in as a returning user', async function () {
     await page.goto(process.env.BASE_URL);
     await page.locator('button[id="my-dashboard"]').click();
     await page.fill('input[name="email"]', 'testuser@ucla.edu');
     await page.fill('input[name="password"]', 'Password123!');
     await page.click('button[type="submit"]');
+});
 
-    // You can add assertions here to verify successful login
+When('I am on the dashboard,', async function () {
     await page.waitForURL(`${process.env.BASE_URL}/dashboard`);
 });
 
 After({ tags: "@sidebar"}, async function () {
     await browser.close();
-})
+});
