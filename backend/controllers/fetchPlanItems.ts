3dcdb85db78b7f64f0c2696792d1a581d8f4dab5
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-12-04 17:48:39 -0800

    refactoring with documentation

--- backend/controllers/fetchPlanItems.ts
+++ backend/controllers/fetchPlanItems.ts
@@ -5,10 +5,8 @@ interface PublicPlanRow extends RowDataPacket {
     plan_id: number,
     major_id: number
 }
-/* Retrieves all of the plans that are shared
-
+/* 
 Returns an array of objects, where each object has a plan_id and major_id
-    e.g.
     [
         { plan_id: 1, major_id: 2 },
         { plan_id: 2, major_id: 3 },
@@ -28,9 +26,8 @@ export async function getAllPublicPlans() {
 }
 
 /*
-Retrieves the name of the major by its major_id
 Returns a row with a single object that has a major_name field
-    e.g. [ { major_name: "Computer Science"} ]
+    [ { major_name: "Computer Science"} ]
 */
 export async function getMajorById(major_id: number) {
     try {
@@ -45,7 +42,6 @@ export async function getMajorById(major_id: number) {
 
 /* Each user has a dedicated plan_id, and each plan_id has a plan_item for each individual course. Given a user's plan_id, return the information of each plan to display on a Course Card
 Returns an array of objects, where each object represents a single course in the user's plan
-    e.g. 
     [
         { plan_item_id: 1, course_number: "MATH 32A", course_name: "Multivariable Calculus", year: 1, quarter: "Fall" },
         { plan_item_id: 2, course_number: "COM SCI 31", course_name: "Introduction to C++", year: 2, quarter: "Spring" },
