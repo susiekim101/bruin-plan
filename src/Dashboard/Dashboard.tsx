2d0eb5e02c670a7e8831386181ea736afbaf5b82
jqin206 <juneqin2017@gmail.com>
2025-12-04 21:39:11 -0800

    save merge conflict resolution in Dashboard.tsx; import type Course from types.ts

--- src/Dashboard/Dashboard.tsx
+++ src/Dashboard/Dashboard.tsx
@@ -7,21 +7,8 @@ import axios from 'axios';
 import { ChevronLeft, ChevronRight, LogOut, House } from "lucide-react";
 import { useContext } from 'react';
 import AuthenticationContext from '../AuthenticationContext.tsx';
-<<<<<<< HEAD
-import type { Course } from './types.ts';
-=======
 import {useVerifyUser, useFetchUserId, useLoadCourses, useSetTotalUnits, handleOpenClick, handleCloseClick, handleLogOut, handleHome, handleLeftClick, handleRightClick } from './DashboardHandler.tsx';
-
-interface Course {
-    course_id: number | null;
-    course_number: string;
-    course_name: string;
-    course_units: number;
-    status: 'Planned' | 'In Progress' | 'Completed';
-    category: string;
-    major_id: number;
-}
->>>>>>> c8a3c2e953e23554863ce61c05b956965efc675a
+import type { Course } from './types.ts';
 
 function Dashboard () {
     const [yearNum, setYearNum] = useState<number>(1);
