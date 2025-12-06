8ea4787c9d3cabe9a8c001b37846c98da09fff75
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-12-05 19:53:50 -0800

    add new password validation check

--- cucumber/steps/user-verification.step.js
+++ cucumber/steps/user-verification.step.js
@@ -27,6 +27,31 @@ Then('I should be able to enter my password', async function () {
     await page.locator('input[name="password"]').fill('Hello6!!');
 });
 
+Given('I am a user who is creating a new account', async function () {
+    browser = await chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    page = await context.newPage();
+    await page.locator('button[id="my-dashboard"]').click();
+    await page.locator('text="Sign up"').click();
+});
+
+When('I enter a weak password, such as Password123', async function (string) {
+    await page.fill('input[name="first-name"]', "Test");
+    await page.fill('input[name="last-name"]',"User");
+    await page.fill('input[name="email"]',"testuser@gmail.com");
+    await page.fill('input[name="password"]',"Password123");
+    await page.selectOption('#major-input', 'Computer Science');
+});
+
+Then('I should not be able to create an account', async function () {
+    const registrationButton = await page.locator('#registration-title').click();
+    expect (registrationButton).toBeDisabled();
+
+});
+
+Then('there should be an error message', async function () {
+    expect(await page.locator('text=["At least one special character from ?!*._"]').isVisible()).toBeFalsy();
+});
 
 After({ tags: "@userVerification"}, async function () {
     await browser.close();
