6779cec0ac543d0c233096ecc69914c7f5a65ff6
Susie Kim <35243112+susiekim101@users.noreply.github.com>
2025-12-05 19:42:16 -0800

    remove comments

--- src/Landing/Login/LogIn.tsx
+++ src/Landing/Login/LogIn.tsx
@@ -35,69 +35,6 @@ function LogIn({ textStyle, px, py }: LogInProps) {
     const { login } = useContext(AuthenticationContext);
 
     useValidateForm(formRef, setIsFormValid);
-    // useEffect(() => {
-    //     const formEl = formRef.current;
-    //     if(!formEl) return;
-
-    //     const validatorInputs = Array.from(
-    //         formEl.querySelectorAll('input.validator, select.validator')
-    //     );
-
-    //     const validate = () => {
-    //         const allValid = validatorInputs.every(input => 
-    //             (input instanceof HTMLInputElement || input instanceof HTMLSelectElement) && input.checkValidity());
-    //         setIsFormValid(allValid);
-    //     }
-
-    //     validatorInputs.forEach(input => {
-    //         input.addEventListener('input', validate);
-    //         input.addEventListener('change', validate);
-    //         input.addEventListener('blur', validate);
-    //     });
-
-    //     validate();
-
-    //     return () => {
-    //         validatorInputs.forEach(input => {
-    //             input.removeEventListener('input', validate);
-    //             input.removeEventListener('change', validate);
-    //             input.removeEventListener('blur', validate);
-    //         });
-    //     };
-    // }, [signup])
-
-    // function handleInputChange(e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) {
-    //     const { name, value } = e.target;
-    //     setUserData((prevData) => ({
-    //         ...prevData,
-    //         [name]: value
-    //     }));
-    // };
-
-    // async function handleSubmit(e: React.FormEvent<HTMLFormElement>): Promise<void> {
-    //     e.preventDefault();
-    //     setError(null);
-    //     if(!isFormValid) {
-    //         console.log("Form is invalid");
-    //         return;
-    //     }
-    //     const url: string = signup ? `http://localhost:3001/user/signup` : 'http://localhost:3001/user/login';
-
-    //     tryLogInSignUp({ navigate, login, setError, url, userData});
-        // try {
-        //     await axios.post(url, userData, { withCredentials: true });
-        //     navigate('/dashboard');
-        //     login();
-        //     return;
-        // } catch (err) {
-        //     if (axios.isAxiosError(err) && err.response && err.response.data === 'Existing email') {
-        //         setError("Account already exists");
-        //     } else {
-        //         setError("User does not exist or incorrect credentials.");
-        //     }
-        //     console.error(`Failed to sign up/log in:`, err);
-        // }
-    // }
 
     return (
         <div className="">
